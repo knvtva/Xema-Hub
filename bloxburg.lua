e49842f4789e4415178a3189c019e6f0beebab05
Ash <87194661+knvtva@users.noreply.github.com>
2022-07-12 16:03:57 +0100

    Changed a value

--- bloxburg.lua
+++ bloxburg.lua
@@ -1,5 +1,7 @@
  if game.PlaceId == 185655149 then -- Bloxburg
     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+    HubGame = "Xema Hub V1 -- Bloxburg"
+
     local Window = Library.CreateLib(HubGame, "Midnight")
     local AutoFarm = Window:NewTab("Auto Farm")
     local AutoBuild = Window:NewTab("Auto Build")
@@ -7,7 +9,6 @@
     local Gamepasses = Window:NewTab("Gamepasses")
     local Credits = Window:NewTab("Credits")
     local PlayerSection = Player:NewSection("Player")
-    HubGame = "Xema Hub V1 -- Bloxburg"
 
     
     PlayerSection:NewSlider("Walkspeed", "Changes the walkspeed of your player", 250, 16, function(v) -- 500 (MaxValue) | 0 (MinValue)
